cfc4f8bfd51dc6400bfe3945e7058b3c50132275
AMITH KUMAR S <144261872+titan-exasaur@users.noreply.github.com>
2024-08-15 20:49:13 +0530

    Create 1_array_characteristics.py

---
+++ 1 NUMPY/1_array_characteristics.py
@@ -0,0 +1,23 @@
+#IMPORTING NECESSARY LIBRARIES
+import numpy as np #used for matrix calculations
+
+#MATRIX EXAMPLE 0
+example_0 = np.array([[1,2],[3,4]])
+
+print("array elements are: ")
+print(example_0)#displaying the created matrix/array
+
+print("type of array is:",type(example_0))#data type of array : type(a)
+
+print("shape of the array :",example_0.shape)#shape of array: a.shape
+
+print("size of the array is:",example_0.size)#size of array: a.size
+
+
+#ASSIGNMENTS 
+'''
+Implement the above code for examples 1, 2, 3 referred to in the youtube video
+'''
+#MATRIX EXAMPLE 1
+#MATRIX EXAMPLE 2
+#MATRIX EXAMPLE 3
